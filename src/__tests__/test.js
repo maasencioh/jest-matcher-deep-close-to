429ecdbf4188de9b54035ac24dc6b2028145953c
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-19 16:50:05 +0200

    test: add failing test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -13,7 +13,25 @@ describe('toBeDeepCloseTo', () => {
     });
 
     it('array of arrays', () => {
-        expect([42]).toBeDeepCloseTo([[42]], 3);
+        expect([[42]]).toBeDeepCloseTo([[42]], 3);
         expect([[42.0003]]).toBeDeepCloseTo([[42.0004]], 3);
+    });
+});
+
+
+describe('fails', () => {
+    it('numbers', () => {
+        expect(43).not.toBeDeepCloseTo(42, 3);
+        expect(42.03).not.toBeDeepCloseTo(42.0004, 3);
+    });
+
+    it('array', () => {
+        expect([43]).not.toBeDeepCloseTo([42], 3);
+        expect([42.03]).not.toBeDeepCloseTo([42.0004], 3);
+    });
+
+    it('array of arrays', () => {
+        expect([[43]]).not.toBeDeepCloseTo([[42]], 3);
+        expect([[42.03]]).not.toBeDeepCloseTo([[42.0004]], 3);
     });
 });
