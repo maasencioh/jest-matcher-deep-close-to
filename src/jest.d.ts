1dd01cb45b42ae3829478840e0429710024dc3f6
Sterling Camden <rusty.camden@gmail.com>
2022-10-03 16:41:20 -0700

    allow the types to override the @jest/expect module
    the old jest way doesn't seem to work in jest 28 but this does.
    
    see: https://jestjs.io/blog/2022/04/25/jest-28
    
    i had a hard to time find a way to share these types, so i duped them for now, but lemme know if you know a way to avoid that.

--- src/jest.d.ts
+++ src/jest.d.ts
@@ -18,3 +18,25 @@ declare namespace jest {
     toMatchCloseTo: (expected: Iterable, decimals?: number) => R;
   }
 }
+
+declare module '@jest/expect' {
+  interface IterableObject {
+    [k: string]: Iterable;
+  }
+  type Iterable =
+    | number
+    | Iterable[]
+    | Float32Array
+    | Float64Array
+    | IterableObject
+    | string
+    | null
+    | undefined
+    | boolean;
+
+  interface Matchers<R> {
+    toBeDeepCloseTo: (expected: Iterable, decimals?: number) => R;
+    toMatchCloseTo: (expected: Iterable, decimals?: number) => R;
+  }
+
+}
